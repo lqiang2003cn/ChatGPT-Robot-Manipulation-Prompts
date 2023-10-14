d1cb65cba1c86115354017f275dd513a93d8c087
lqiang2003cn <randycoq@163.com>
2023-10-15 00:11:31 +0800

    c

--- examples/task_decomposition_virtualhome/lq_test/virtual_home_test.py
+++ examples/task_decomposition_virtualhome/lq_test/virtual_home_test.py
@@ -37,14 +37,14 @@ fridge_node = [node for node in graph['nodes'] if node['class_name'] == 'fridge'
 fridge_node['states'] = ['OPEN']
 
 # create a new node
-new_node = {
-    'id': 1000,
-    'class_name': 'salmon',
-    'states': []
-}
+# new_node = {
+#     'id': 1000,
+#     'class_name': 'salmon',
+#     'states': []
+# }
 # Add an edge
-new_edge = {'from_id': 1000, 'to_id': fridge_node['id'], 'relation_type': 'INSIDE'}
-graph['nodes'].append(new_node)
+new_edge = {'from_id': 327, 'to_id': fridge_node['id'], 'relation_type': 'INSIDE'}
+# graph['nodes'].append(new_node)
 graph['edges'].append(new_edge)
 
 # update the environment
@@ -62,11 +62,9 @@ for i, im in enumerate(images):
 # Reset the environment
 comm.reset(0)
 comm.add_character('Chars/Female2')
-
 # Get nodes for salmon and microwave
 salmon_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'salmon'][0]
 microwave_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'microwave'][0]
-
 # Put salmon in microwave
 script = [
     '<char0> [walk] <salmon> ({})'.format(salmon_id),
@@ -76,3 +74,50 @@ script = [
     '<char0> [close] <microwave> ({})'.format(microwave_id)
 ]
 comm.render_script(script, recording=True, frame_rate=10)
+
+# # Reset the environment
+# comm.reset(0)
+# # Add two agents this time
+# comm.add_character('Chars/Male2', initial_room='kitchen')
+# comm.add_character('Chars/Female4', initial_room='bedroom')
+# # Get nodes for salmon and microwave, glass, faucet and sink
+# salmon_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'salmon'][0]
+# microwave_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'microwave'][0]
+# glass_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'waterglass'][-1]
+# sink_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'sink'][0]
+# faucet_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'faucet'][-1]
+# # Put salmon in microwave
+# script = [
+#     '<char0> [walk] <salmon> ({}) | <char1> [walk] <glass> ({})'.format(salmon_id, glass_id),
+#     '<char0> [grab] <salmon> ({}) | <char1> [grab] <glass> ({})'.format(salmon_id, glass_id),
+#     '<char0> [open] <microwave> ({}) | <char1> [walk] <sink> ({})'.format(microwave_id, sink_id),
+#     '<char0> [putin] <salmon> ({}) <microwave> ({}) | <char1> [putback] <glass> ({}) <sink> ({})'.format(salmon_id, microwave_id, glass_id, sink_id),
+#     '<char0> [close] <microwave> ({}) | <char1> [switchon] <faucet> ({})'.format(microwave_id, faucet_id)
+# ]
+# comm.render_script(script, recording=True, frame_rate=10, camera_mode=["PERSON_FROM_BACK"])
+
+# Reset the environment
+comm.reset(0)
+# Add two agents this time
+comm.add_character('Chars/Male2', initial_room='kitchen')
+comm.add_character('Chars/Female4', initial_room='bedroom')
+# Get nodes for salmon and microwave, glass, faucet and sink
+salmon_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'salmon'][0]
+microwave_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'microwave'][0]
+glass_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'waterglass'][-1]
+sink_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'sink'][0]
+faucet_id = [node['id'] for node in graph['nodes'] if node['class_name'] == 'faucet'][-1]
+# Put salmon in microwave
+script = [
+    '<char0> [walk] <salmon> ({}) | <char1> [walk] <glass> ({})'.format(salmon_id, glass_id),
+    '<char0> [grab] <salmon> ({}) | <char1> [grab] <glass> ({})'.format(salmon_id, glass_id),
+    '<char0> [open] <microwave> ({}) | <char1> [walk] <sink> ({})'.format(microwave_id, sink_id),
+    '<char0> [putin] <salmon> ({}) <microwave> ({}) | <char1> [putback] <glass> ({}) <sink> ({})'.format(salmon_id, microwave_id, glass_id, sink_id),
+    '<char0> [close] <microwave> ({}) | <char1> [switchon] <faucet> ({})'.format(microwave_id, faucet_id)
+]
+
+s, cc = comm.camera_count()
+for script_instruction in script:
+    comm.render_script([script_instruction], recording=False, skip_animation=True)
+    # Here you can get an observation, for instance
+    s, im = comm.camera_image([cc-3])
